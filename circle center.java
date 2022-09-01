9a41d4a6fc3378f432180b766206d80244c65e04
sumeettiwari26011999 <109622996+sumeettiwari26011999@users.noreply.github.com>
2022-09-01 20:59:40 +0530

    Create circle center.java

---
+++ circle center.java
@@ -0,0 +1,22 @@
+
+// Question number two
+class Circle{
+	public double center;
+	public double radius;
+
+	public Circle(double center, double radius){
+		this.center = center;
+		this.radius = radius;
+	}
+
+	public Circle(){
+		System.out.println("The radius of circle is "+radius +" and the center is "+ center);
+	}
+
+
+public class Que02{
+	public static void main(String args[]){
+		Circle c = new Circle(4.0, 5.9);
+		new Circle();
+	}
+}
